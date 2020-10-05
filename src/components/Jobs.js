96fec3ab7a4ac7031328130b1cde5695466404a2
Hari.Doshi <hdoshi2@gmail.com>
2020-10-04 21:44:47 -0400

    ADD: Begin connecting to Strapi

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -4,7 +4,28 @@ import { FaAngleDoubleRight } from "react-icons/fa"
 import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
 
+
+const query = graphql`
+  {
+    allStrapiJobs(sort: {fields: strapiId, order: DESC}) {
+      nodes {
+        company
+        date
+        desc {
+          id
+          name
+        }
+        strapiId
+        position
+      }
+    }
+  }
+`
+
+
 const Jobs = () => {
+  const data = useStaticQuery(query);
+  console.log(data);
   return <h2>jobs component</h2>
 }
 
