e133f806bc9628a25c433f536ce782d623f8c5fc
Hari.Doshi <hdoshi2@gmail.com>
2020-10-17 18:33:44 -0400

    ADD job component

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -24,9 +24,53 @@ const query = graphql`
 
 
 const Jobs = () => {
+
   const data = useStaticQuery(query);
-  console.log(data);
-  return <h2>jobs component</h2>
+  const {
+    allStrapiJobs: { nodes: jobs },
+  } = data;
+  const [value, setValue] = React.useState(0);
+  const { company, position, date, desc } = jobs[value];
+
+  return <section className="section jobs">
+    <Title title="experience" />
+    <div className="jobs-center">
+
+      {/* btn container */}
+      <div className="btn-container">
+        {jobs.map((item, index) => {
+          return (
+            <button
+              key={item.strapiId}
+              onClick={() => setValue(index)}
+              className={`job-btn ${index === value && "active-btn"}`}>
+              {item.company}
+            </button>)
+        })}
+      </div>
+
+      {/* job info */}
+      <article className="job-info">
+        <h3>{position}</h3>
+        <h4>{company}</h4>
+        <p className="job-date">{date}</p>
+        {
+          desc.map((item) => {
+            return <div key={item.id} className="job-desc">
+              <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+              <p>{item.name}</p>
+            </div>
+          })
+        }
+      </article>
+
+    </div>
+
+    <Link to="/about" className="btn center-btn">
+      More Info
+    </Link>
+
+  </section>
 }
 
 export default Jobs
