4468c834502e48652a93eb71a85ae6cd2ebf24ef
Hari.Doshi <hdoshi2@gmail.com>
2020-10-17 21:38:34 -0400

    ADD projects component

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,43 @@ import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
 
-export default () => {
+export default ({ data }) => {
+
+  console.log("data", data);
+  const { allStrapiProjects: { nodes: projects } } = data;
+
   return <Layout>
     <Hero />
     <Services />
     <Jobs />
+    <Projects projects={projects} title="Featured Projects" showLink />
   </Layout>
 }
+
+export const query = graphql`
+  {
+    allStrapiProjects(filter: {featured: {eq: true}}) {
+      nodes {
+        github
+        id
+        title
+        url
+        description
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+`
+
+
 // ...GatsbyImageSharpFluid
