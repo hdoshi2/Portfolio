f808bcb03bc6d3528579dac6702e4692d0f21600
Hari.Doshi <hdoshi2@gmail.com>
2020-10-17 22:02:33 -0400

    ADD link to view all projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,22 +9,22 @@ const Project = ({ description, title, github, stack, url, image, index }) => {
   return (<article className="project">
     <Image fluid={image.childImageSharp.fluid} className="project-img" />
     <div className="project-info">
-      <span className="project-number">0{index + 1}</span>
+      <span className="project-number">0{index + 1}.</span>
       <h3>{title}</h3>
       <p className="project-desc">
         {description}
       </p>
       <div className="project-stack">
-        {stack.map((item)=>{
+        {stack.map((item) => {
           return <span key={item.id}>{item.title}</span>
         })}
       </div>
       <div className="project-links">
         <a href={github}>
-          <FaGithubSquare className="project-icon"/>
+          <FaGithubSquare className="project-icon" />
         </a>
         <a href={url}>
-          <FaShareSquare className="project-icon"/>
+          <FaShareSquare className="project-icon" />
         </a>
       </div>
     </div>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,10 +2,41 @@ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Projects from "../components/Projects"
-// ...GatsbyImageSharpFluid
 
-const ProjectsPage = () => {
-  return <h2>projects page</h2>
+export const query = graphql`
+  {
+    allStrapiProjects {
+      nodes {
+        github
+        id
+        title
+        url
+        description
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+`
+
+const ProjectsPage = ({ data }) => {
+
+  const { allStrapiProjects: { nodes: projects } } = data;
+
+  return <Layout>
+    <section className="projects-page">
+      <Projects projects={projects} title="All Projects" />
+    </section>
+  </Layout>
 }
 
 export default ProjectsPage
